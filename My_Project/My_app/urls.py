e7cb39e676f27102fe13cd047d77360788a26ac6
AkankshaKaple <akankshakaple@gmail.com>
2019-12-30 00:59:26 +0530

    Refactor: removed my_project folder

--- My_Project/My_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-
-    path('', views.dynamic_page, name='dynamic_page'),
-    path('', views.static_page, name='static_page')
-]
